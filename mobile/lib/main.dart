5ed2d8d9167c221c26d0100310e3a4be50eb7dd0
Patrick Melaga <patrickmelago17@gmail.com>
2026-09-15 13:12:01 +0100

    fix(android): suppression de l'overlay natif Android 12 Splash pour un démarrage instantané du moteur Flutter

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  // Instantly remove native Android/iOS splash screen to transition to Flutter UI
-  FlutterNativeSplash.remove();
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     const ProviderScope(
