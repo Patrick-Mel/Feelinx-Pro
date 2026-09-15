a23fb0c267e94b85d49b950dd86bdcef67b8eb22
Patrick Melaga <patrickmelago17@gmail.com>
2026-09-15 13:05:14 +0100

    fix(splash): intégration de FlutterNativeSplash.remove() dans main.dart pour débloquer définitivement le splash screen Android 12+

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // Instantly remove native Android/iOS splash screen to transition to Flutter UI
+  FlutterNativeSplash.remove();
 
   runApp(
     const ProviderScope(
